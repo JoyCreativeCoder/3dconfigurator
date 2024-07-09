12553472d1012eaa784cd7913d9a1c9b8f0a6a1a
creativecoderjoy <154061032+creativecoderjoy@users.noreply.github.com>
2024-07-09 12:38:37 +0100

    load 3d model

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,27 +1,40 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, Center, useGLTF } from '@react-three/drei';
+import { useRef } from 'react'
 
-function Box() {
+function Box(props) {
+  const { nodes, materials } = useGLTF('/3d_shirt.glb')
   return (
-    <mesh rotation={[90, 0, 20]}>
-      <boxGeometry args={[2, 2, 2]} />
-      <meshStandardMaterial color={'orange'} />
-    </mesh>
-  );
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.T_Shirt_male.geometry}
+        material={materials.lambert1}
+        position={[0.419, 0, 0]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+    </group>
+  )
 }
 
-function CanvasApp() { // Renamed to avoid conflict with Canvas from @react-three/fiber
+export const CanvasApp = ({ position = [-1, 0, 2.5], fov = 25 }) => {
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <Canvas>
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[5, 5, 5]} />
+    <Canvas camera={{ position, fov }}
+    eventSource={document.getElementById('root')}
+    eventPrefix="client"
+    >
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[5, 5, 5]} />
+      <Center>
         <Box />
-        <OrbitControls />
-      </Canvas>
-    </div>
+      </Center>
+      <OrbitControls />
+    </Canvas>
   );
-}
+};
 
 export default CanvasApp;
+
+useGLTF.preload('/3d_shirt.glb')
