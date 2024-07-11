5c8cea6ecb88fb61e9c36faa776cfc17725c08d7
creativecoderjoy <154061032+creativecoderjoy@users.noreply.github.com>
2024-07-11 16:45:08 +0100

    add subtle animtion with framer motion and adjust styling

--- src/Overlay.js
+++ src/Overlay.js
@@ -9,25 +9,44 @@ import {
 
 import { useSnapshot } from "valtio";
 import { state } from "./Store";
+import { motion, AnimatePresence } from 'framer-motion'
 
 export default function Overlay() {
   const snap = useSnapshot(state);
 
+  const transition = { type: 'spring', duration: 0.8 }
+
+  const config = {
+    initial: { x: -100, opacity: 0, transition: { ...transition, delay: 0.5 } },
+    animate: { x: 0, opacity: 1, transition: { ...transition, delay: 0 } },
+    exit: { x: -100, opacity: 0, transition: { ...transition, delay: 0 } }
+  }
+
   return (
     <div className="container">
-      <header>
+      <header
+             initial={{ opacity: 0, y: -120 }}
+             animate={{ opacity: 1, y: 0 }}
+             transition={{ type: 'spring', duration: 1.8, delay: 1 }}
+      >
         <SimpleLogo />
         <AiOutlineShopping size="3em" />
       </header>
 
-      {snap.intro ? <Intro /> : <Customiser />}
+      <AnimatePresence>
+        {snap.intro ? (
+           <Intro key="main" config={config} /> 
+        ) : (
+          <Customiser key="custom" config={config} />
+        )}
+      </AnimatePresence>
     </div>
   );
 }
 
-function Intro() {
+function Intro({ config }) {
   return (
-    <section key="main">
+    <motion.section {...config}>
       <div className="section--container">
         <div>
           <h1>LET'S DO IT.</h1>
@@ -48,11 +67,11 @@ function Intro() {
           </div>
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 }
 
-function Customiser() {
+function Customiser({ config }) {
   const snap = useSnapshot(state);
 
   const colors = [
@@ -68,7 +87,7 @@ function Customiser() {
   const decals = ["react", "three2", "pmndrs"];
 
   return (
-    <section key="custom">
+    <motion.section {...config}>
       <div className="customizer">
         <div className="color-options">
           {colors.map((color) => (
@@ -126,6 +145,6 @@ function Customiser() {
           <AiOutlineArrowLeft size="1.3em" />
         </button>
       </div>
-    </section>
+    </motion.section>
   );
 }
